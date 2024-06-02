605077342abac31622787a340bdadfcc6a5ef044
Zoruasy <145676379+Zoruasy@users.noreply.github.com>
2024-06-02 14:14:14 +0200

    Update animated.js

--- src/js/animated.js
+++ src/js/animated.js
@@ -19,24 +19,24 @@ export class Animated extends Actor {
                 { graphic: Resources.Fly06.toSprite(), duration: 100 },
                 { graphic: Resources.Fly07.toSprite(), duration: 100 }
             ],
-            loop: true // Make the animation loop
+            loop: true 
         });
 
         this.graphics.use(animation);
     }
 
     onInitialize(engine) {
-        // Set the initial velocity of the animated sprite
+      
         this.vel = new Vector(100, 0); // Move to the right at 100 pixels per second
     }
 
     update(engine, delta) {
         super.update(engine, delta);
 
-        // Update the position based on velocity
-        this.pos.x += this.vel.x * delta / 1000; // Convert velocity from pixels per second to pixels per millisecond
+       
+        this.pos.x += this.vel.x * delta / 1000; 
 
-        // Update animation position to match actor's position
+       
         this.graphics.x = this.pos.x;
         this.graphics.y = this.pos.y;
     }
